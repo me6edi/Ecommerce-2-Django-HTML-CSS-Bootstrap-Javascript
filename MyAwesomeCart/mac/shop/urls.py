d5ce7e1ffe083766617326081111d5e9a94cfe5e
me6edi <exited17@gmail.com>
2020-09-04 11:43:30 +0600

    shop field add

--- MyAwesomeCart/mac/shop/urls.py
+++ MyAwesomeCart/mac/shop/urls.py
@@ -3,4 +3,10 @@ from django.urls import path
 from . import views
 urlpatterns = [
 path('', views.index, name="ShopHome"),
+path('about/', views.about, name="aboutUs"),
+path('contact/', views.contact, name="contactUs"),
+path('tracker/', views.tracker, name="TrackingStatus"),
+path('search/', views.search, name="Search"),
+path('productview/', views.productView, name="Productview"),
+path('checkout/', views.checkout, name="Checkout"),
 ]
