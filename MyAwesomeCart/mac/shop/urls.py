296ad12c803281b4287b2b866a721e745eefcec8
me6edi <exited17@gmail.com>
2020-09-22 18:20:45 +0800

    add about page

--- MyAwesomeCart/mac/shop/urls.py
+++ MyAwesomeCart/mac/shop/urls.py
@@ -1,12 +1,12 @@
-
 from django.urls import path
 from . import views
+
 urlpatterns = [
-path('', views.index, name="ShopHome"),
-path('about/', views.about, name="aboutUs"),
-path('contact/', views.contact, name="contactUs"),
-path('tracker/', views.tracker, name="TrackingStatus"),
-path('search/', views.search, name="Search"),
-path('productview/', views.productView, name="Productview"),
-path('checkout/', views.checkout, name="Checkout"),
-] 
+    path("", views.index, name="ShopHome"),
+    path("about/", views.about, name="AboutUs"),
+    path("contact/", views.contact, name="ContactUs"),
+    path("tracker/", views.tracker, name="TrackingStatus"),
+    path("search/", views.search, name="Search"),
+    path("products/<int:myid>", views.productView, name="ProductView"),
+    path("checkout/", views.checkout, name="Checkout"),
+]
