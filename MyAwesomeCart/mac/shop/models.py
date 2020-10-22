5d7e3b3243055b09d452852c435309fe2c188675
me6edi <exited17@gmail.com>
2020-10-22 21:08:11 +0800

    Shop page eite admin section

--- MyAwesomeCart/mac/shop/models.py
+++ MyAwesomeCart/mac/shop/models.py
@@ -27,15 +27,16 @@ class Contact(models.Model):
         return self.name
 
 class Orders(models.Model):
-    order_id = models.AutoField(primary_key=True)
-    items_json = models.CharField(max_length=5000)
-    name = models.CharField(max_length=90)
-    email = models.CharField(max_length=111)
-    address = models.CharField(max_length=111)
-    city = models.CharField(max_length=111)
-    state = models.CharField(max_length=111)
-    zip_code = models.CharField(max_length=111)
-    phone = models.CharField(max_length=111, default="")
+    order_id= models.AutoField(primary_key=True)
+    items_json= models.CharField(max_length=5000)
+    amount=models.IntegerField(default=0)
+    name=models.CharField(max_length=90)
+    email=models.CharField(max_length=111)
+    address=models.CharField(max_length=111)
+    city=models.CharField(max_length=111)
+    state=models.CharField(max_length=111)
+    zip_code=models.CharField(max_length=111)
+    phone=models.CharField(max_length=111, default="")
 
 class OrderUpdate(models.Model):
     update_id= models.AutoField(primary_key=True)
